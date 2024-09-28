78ff6a77bbafe2c8ecdea6ad8f0ad13b70ad652d
polzbit <bar.polyak2@gmail.com>
2024-09-28 09:42:47 +0300

    added hide controls logic

--- KeepAlive/widgets/TasksTab.py
+++ KeepAlive/widgets/TasksTab.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton
 from PyQt5.QtCore import Qt, pyqtSignal, QTimer
-from PyQt5.QtGui import  QColor
+from PyQt5.QtGui import  QColor, QIcon
 from KeepAlive.core.calendar import get_runtime_seconds
 import pandas as pd
 from datetime import datetime
@@ -11,15 +11,16 @@ class TasksTab(QWidget):
     onDelete = pyqtSignal(int)
     def __init__(self):
         super().__init__()
+        self.show_controls = True
         self.initUI()
     
     def initUI(self):
-        price_text_label = QLabel()
-        price_text_label.setText("Factor")
-        price_text_label.setMinimumSize(100, 20)
-        price_text_label.setMaximumSize(100, 20)
-        price_text_label.setAlignment(Qt.AlignLeft)
-        price_text_label.setContentsMargins(0, 0, 0, 0)
+        self.price_text_label = QLabel()
+        self.price_text_label.setText("Factor")
+        self.price_text_label.setMinimumSize(100, 20)
+        self.price_text_label.setMaximumSize(100, 20)
+        self.price_text_label.setAlignment(Qt.AlignLeft)
+        self.price_text_label.setContentsMargins(0, 0, 0, 0)
 
         self.price_label = QLineEdit()
         self.price_label.setText("200")
@@ -29,12 +30,12 @@ class TasksTab(QWidget):
         self.price_label.setContentsMargins(0, 0, 0, 0)
         self.price_label.textChanged.connect(self.on_factor_change)
 
-        hours_label = QLabel()
-        hours_label.setText("Total hours")
-        hours_label.setMinimumSize(95, 20)
-        hours_label.setMaximumSize(95, 20)
-        hours_label.setAlignment(Qt.AlignLeft)
-        hours_label.setContentsMargins(0, 0, 0, 0)
+        self.hours_label = QLabel()
+        self.hours_label.setText("Total hours")
+        self.hours_label.setMinimumSize(95, 20)
+        self.hours_label.setMaximumSize(95, 20)
+        self.hours_label.setAlignment(Qt.AlignLeft)
+        self.hours_label.setContentsMargins(0, 0, 0, 0)
 
         self.total_hours_label = QLineEdit()
         self.total_hours_label.setText("0")
@@ -44,12 +45,12 @@ class TasksTab(QWidget):
         self.total_hours_label.setContentsMargins(0, 0, 0, 0)
         self.total_hours_label.setReadOnly(True)
 
-        monthly_label = QLabel()
-        monthly_label.setText("Monthly total")
-        monthly_label.setMinimumSize(95, 20)
-        monthly_label.setMaximumSize(95, 20)
-        monthly_label.setAlignment(Qt.AlignLeft)
-        monthly_label.setContentsMargins(0, 0, 0, 0)
+        self.monthly_label = QLabel()
+        self.monthly_label.setText("Monthly total")
+        self.monthly_label.setMinimumSize(95, 20)
+        self.monthly_label.setMaximumSize(95, 20)
+        self.monthly_label.setAlignment(Qt.AlignLeft)
+        self.monthly_label.setContentsMargins(0, 0, 0, 0)
 
         self.total_monthly = QLineEdit()
         self.total_monthly.setText("0")
@@ -59,12 +60,12 @@ class TasksTab(QWidget):
         self.total_monthly.setContentsMargins(0, 0, 0, 0)
         self.total_monthly.setReadOnly(True)
 
-        paid_text_label = QLabel()
-        paid_text_label.setText("Paid")
-        paid_text_label.setMinimumSize(200, 20)
-        paid_text_label.setMaximumSize(200, 20)
-        paid_text_label.setAlignment(Qt.AlignLeft)
-        paid_text_label.setContentsMargins(0, 0, 0, 0)
+        self.paid_text_label = QLabel()
+        self.paid_text_label.setText("Paid")
+        self.paid_text_label.setMinimumSize(200, 20)
+        self.paid_text_label.setMaximumSize(200, 20)
+        self.paid_text_label.setAlignment(Qt.AlignLeft)
+        self.paid_text_label.setContentsMargins(0, 0, 0, 0)
 
         self.paid_label = QLineEdit()
         self.paid_label.setText("0")
@@ -74,12 +75,12 @@ class TasksTab(QWidget):
         self.paid_label.setContentsMargins(0, 0, 0, 0)
         self.paid_label.setReadOnly(True)
 
-        sum_text_label = QLabel()
-        sum_text_label.setText("Remaining")
-        sum_text_label.setMinimumSize(200, 20)
-        sum_text_label.setMaximumSize(200, 20)
-        sum_text_label.setAlignment(Qt.AlignLeft)
-        sum_text_label.setContentsMargins(0, 0, 0, 0)
+        self.sum_text_label = QLabel()
+        self.sum_text_label.setText("Remaining")
+        self.sum_text_label.setMinimumSize(200, 20)
+        self.sum_text_label.setMaximumSize(200, 20)
+        self.sum_text_label.setAlignment(Qt.AlignLeft)
+        self.sum_text_label.setContentsMargins(0, 0, 0, 0)
 
         self.sum_label = QLineEdit()
         self.sum_label.setText("0")
@@ -89,31 +90,70 @@ class TasksTab(QWidget):
         self.sum_label.setContentsMargins(0, 0, 0, 0)
         self.sum_label.setReadOnly(True)
 
+        self.show_button = QPushButton()
+        self.show_button.setText('Show')
+        self.show_button.setMinimumSize(50, 30)
+        self.show_button.setMaximumSize(50, 30)
+        self.show_button.setContentsMargins(0, 0, 0, 0)
+        self.show_button.clicked.connect(self.toggleControls)
+        
         self.tasks_table = TasksTable()
         self.onDelete = self.tasks_table.onDelete
         self.onUpdateCell = self.tasks_table.onUpdateCell
         self.tasks_table.onTotalHoursChange.connect(self.on_total_hours_change)
 
+        layout_top = QHBoxLayout()
+        layout_top.addWidget(self.show_button)
+
         controls_layout_top = QHBoxLayout()
-        controls_layout_top.addWidget(price_text_label)
+        controls_layout_top.addWidget(self.price_text_label)
         controls_layout_top.addWidget(self.price_label)
-        controls_layout_top.addWidget(paid_text_label)
+        controls_layout_top.addWidget(self.paid_text_label)
         controls_layout_top.addWidget(self.paid_label)
-        controls_layout_top.addWidget(sum_text_label)
+        controls_layout_top.addWidget(self.sum_text_label)
         controls_layout_top.addWidget(self.sum_label)
 
         controls_layout = QHBoxLayout()
-        controls_layout.addWidget(hours_label)
+        controls_layout.addWidget(self.hours_label)
         controls_layout.addWidget(self.total_hours_label)
-        controls_layout.addWidget(monthly_label)
+        controls_layout.addWidget(self.monthly_label)
         controls_layout.addWidget(self.total_monthly)
         controls_layout.addSpacing(600)
 
         layout = QVBoxLayout()
+        layout.addLayout(layout_top)
         layout.addLayout(controls_layout_top)
         layout.addLayout(controls_layout)
         layout.addWidget(self.tasks_table)
         self.setLayout(layout)
+        self.toggleControls()
+
+    def toggleControls(self):
+        self.show_controls = not self.show_controls
+        if self.show_controls:
+            self.show_button.setText('Hide')
+            self.price_text_label.show()
+            self.price_label.show()
+            self.paid_text_label.show()
+            self.paid_label.show()
+            self.sum_text_label.show()
+            self.sum_label.show()
+            self.hours_label.show()
+            self.total_hours_label.show()
+            self.monthly_label.show()
+            self.total_monthly.show()
+        else:
+            self.show_button.setText('Show')
+            self.price_text_label.hide()
+            self.price_label.hide()
+            self.paid_text_label.hide()
+            self.paid_label.hide()
+            self.sum_text_label.hide()
+            self.sum_label.hide()
+            self.hours_label.hide()
+            self.total_hours_label.hide()
+            self.monthly_label.hide()
+            self.total_monthly.hide()
 
     def on_factor_change(self, text: str):
         if text.isnumeric():
